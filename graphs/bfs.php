ecbb30f9d8407a05c28d4cdf6d067b7ab72806f5
Minnur Yunusov <mmyunusov@gmail.com>
2017-05-13 00:20:52 -0700

    Add Graph Breadth First Search algorithm queue implementation.

---
+++ graphs/bfs.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Graph Breadth First Search.
+ *   Breath first search is an approach of passing through all 
+ *   the neighbors of the node first, and then go to the next level.
+ *
+ * Algorithm:
+ *  Initially we mark all vertices as unvisited. A common approach is to create 
+ *  an empty queue where we put the vertices level by level, starting with the initial vertex.
+ * 
+ * Performance:
+ *   The complexity of this algorithm clearly is O(n + m).
+ */
+
+class Graph {
+
+  protected $visited = [];
+  protected $matrix = [];
+  protected $length = 0;
+
+  public function __construct(array $matrix) {
+    $this->matrix = $matrix;
+    $this->length = count($this->matrix);
+    $this->visited = array_fill(0, $this->length, 0);
+  }
+
+  public function breadthFirst(int $initial_vertex) {
+    $queue = [];
+    array_push($queue, $initial_vertex);
+    $this->visited[$initial_vertex] = 1;
+    echo $initial_vertex . "\n";
+    while (count($queue)) {
+      $t = array_shift($queue);
+      foreach ($this->matrix[$t]  as $key => $vertex) {
+        if (!$this->visited[$key] && $vertex == 1) {
+          $this->visited[$key] = 1;
+          array_push($queue, $key);
+          echo $key . "\t";
+        }
+      }
+      echo "\n";
+    }
+  }
+
+}
+
+$matrix = [
+  [0, 1, 1, 0, 0, 0],
+  [1, 0, 0, 1, 0, 0],
+  [1, 0, 0, 1, 1, 1],
+  [0, 1, 1, 0, 1, 0],
+  [0, 0, 1, 0, 0, 1],
+  [0, 0, 1, 0, 1, 0],
+];
+
+$search = new Graph($matrix);
+$search->breadthFirst(5);
